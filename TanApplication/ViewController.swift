83bbdf3364ec1aac0aed7c51bb3f91c5ce631ed3
Salomé Russier <salomerussier@MacBook-Pro-de-Salome.local>
2016-12-07 12:22:14 +0100

    Initial Commit

---
+++ TanApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TanApplication
+//
+//  Created by Salomé Russier on 07/12/2016.
+//  Copyright © 2016 Salomé Russier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
